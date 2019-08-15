83fb267d06e8884af8c1f8cfbd1c29d42208fe5e
Dominik Rutter <dominik.rutter@gmail.com>
2019-08-15 03:20:12 -0400

    Menu screen with interactable button

--- src/gameStates/MenuScreen.cpp
+++ src/gameStates/MenuScreen.cpp
@@ -2,18 +2,28 @@
 #include "MenuScreen.h"
 
 void MenuScreen::run(sf::RenderWindow & window) {
-	sf::RectangleShape rect(sf::Vector2f(1920, 1080));
+	sf::RectangleShape rect(sf::Vector2f(400, 200));
+	rect.setPosition(1000, 500);
 	window.clear();
 	window.draw(rect);
 	window.display();
 	sf::Event evnt;
 	while (true) {
 		while (window.pollEvent(evnt)) {
-			switch (evnt.type)
-			{
-			case sf::Event::KeyPressed:
-			case sf::Event::MouseButtonPressed:
-				return;
+			if (evnt.type == sf::Event::KeyPressed) {
+				rect.setFillColor(sf::Color::Blue);
+				window.clear();
+				window.draw(rect);
+				window.display();
+			} else if (evnt.type == sf::Event::MouseButtonPressed) {
+				sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+				sf::Vector2f mousePosF(static_cast<float>(mousePos.x), static_cast<float>(mousePos.y));
+				if (rect.getGlobalBounds().contains(mousePosF)) {
+					rect.setFillColor(sf::Color::Red);
+					window.clear();
+					window.draw(rect);
+					window.display();
+				}
 			}
 		}
 	}
