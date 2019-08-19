1ca9aeb3fa5039b898c984264cb30841da217377
Dominik Rutter <dominik.rutter@gmail.com>
2019-08-18 23:34:22 -0400

    Menu Screen (Main Menu) big update

--- src/gameStates/MenuScreen.cpp
+++ src/gameStates/MenuScreen.cpp
@@ -1,67 +1,114 @@
+#include "MenuScreen.hpp"
 #include "../game.hpp"
 #include <SFML/Graphics.hpp>
-#include "MenuScreen.hpp"
 #include "../player/player.hpp"
+#include "../customObjects/menuButton.hpp"
 
 void MenuScreen::run(sf::RenderWindow& window, GameState& state, Player& plr) {
-	bool accBoxActive = false;
-	sf::Texture pfpT;
-	sf::RectangleShape pfp(sf::Vector2f(200, 200));
-	pfp.setPosition(500, 500);
-	sf::RectangleShape rect(sf::Vector2f(400, 200));
-	rect.setPosition(1000, 500);
-	sf::RectangleShape accountButton(sf::Vector2f(400, 200));
-	accountButton.setPosition(500, 250);
-	sf::Font agencyFont;
-	if (!agencyFont.loadFromFile("C:/Windows/Fonts/AGENCYB.TTF"));
-	sf::Text accountText("", agencyFont, 20);
-	accountText.setPosition(accountButton.getPosition().x, accountButton.getPosition().y);
-	accountText.setColor(sf::Color::Green);
-	sf::String accountTextS;
+
+	enum selection {
+		Play,
+		Exit,
+		Nothing
+	};
+	selection hoveredSelection = selection::Nothing;
+	selection enteredSelection = selection::Nothing;
+	
+	std::vector<sf::Drawable*> screenElements;
+	std::vector<menuButton> buttons;
+	
+	bool exiting = false;
+
+	sf::Texture bg;
+	if (bg.loadFromFile("assets/mainMenu/menuBackground.png") != true) {
+		return;
+	}
+	sf::Sprite menuBg(bg);
+	screenElements.push_back(&menuBg);
+	
+	menuButton exitButton("Exit", sf::Vector2f(200, 100), sf::Vector2f(window.getSize().x / 2, (window.getSize().y / 5) * 4));
+	screenElements.push_back(&exitButton);
+	buttons.push_back(exitButton);
+	
+	menuButton playButton("Play", sf::Vector2f(200, 100), sf::Vector2f(window.getSize().x / 2, (window.getSize().y / 5) * 2));
+	screenElements.push_back(&playButton);
+	buttons.push_back(playButton);
+
+	sf::CircleShape activeButton(10);
+	activeButton.setFillColor(sf::Color::Red);
+	activeButton.setOrigin(10 / 2, 10 / 2);
+	activeButton.setPosition(((exitButton.getPos().x) - (exitButton.getSize().x / 2) - 10), (exitButton.getPos().y) - exitButton.getSize().y / 2);
+	screenElements.push_back(&activeButton);
 	
 	sf::Event evnt;
-	while (true) {
+	while (!exiting) {
+		// Event Processor
 		while (window.pollEvent(evnt)) {
-			if (evnt.type == sf::Event::TextEntered) {
-				if (accBoxActive) {
-					if (evnt.text.unicode == '\b') {
-						accountTextS.erase(accountTextS.getSize() - 1, 1);
-						accountText.setString(accountTextS);
-					}
-					else if (evnt.text.unicode < 128) {
-						accountTextS += evnt.text.unicode;
-						accountText.setString(accountTextS);
+			switch (evnt.type)
+			{
+			case sf::Event::KeyPressed: {
+					switch (evnt.key.code)
+					{
+					case sf::Keyboard::Up:
+						if (hoveredSelection > 0) {
+						}
+					case sf::Keyboard::Down:
+						if (hoveredSelection < 1) {
+
+						}
+					case sf::Keyboard::Enter:
+						enteredSelection = hoveredSelection;
+					case sf::Keyboard::Escape:
+						enteredSelection = selection::Exit;
 					}
 				}
-				rect.setFillColor(sf::Color::Blue);
-			} else if (evnt.type == sf::Event::KeyPressed) {
-				if (evnt.key.code == sf::Keyboard::Escape) {
-					state = Exiting;
-					return;
-				} else if (evnt.key.code == sf::Keyboard::Enter) {
-					std::string s = accountTextS;
-					plr.loadPlayer(s);
-					pfpT.loadFromImage(*plr.getPlrPfp());
-					pfp.setTexture(&pfpT);
+			case sf::Event::MouseButtonPressed: {
+					sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+					sf::Vector2f mousePosF(static_cast<float>(mousePos.x), static_cast<float>(mousePos.y));
+					for (auto& element : buttons) {
+						if (element.getBounds().contains(mousePosF)) {
+							if (element.getName() == "Play") {
+								enteredSelection = selection::Play;
+							}
+							else if (element.getName() == "Exit") {
+								enteredSelection = selection::Exit;
+							}
+						}
+					}
 				}
-			} else if (evnt.type == sf::Event::MouseButtonPressed) {
-				sf::Vector2i mousePos = sf::Mouse::getPosition(window);
-				sf::Vector2f mousePosF(static_cast<float>(mousePos.x), static_cast<float>(mousePos.y));
-				if (rect.getGlobalBounds().contains(mousePosF)) {
-					rect.setFillColor(sf::Color::Red);
-					accBoxActive = false;
-				} else if (accountButton.getGlobalBounds().contains(mousePosF)) {
-					accBoxActive = true;
-				} else {
-					accBoxActive = false;
+			case sf::Event::MouseMoved: {
+					sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+					sf::Vector2f mousePosF(static_cast<float>(mousePos.x), static_cast<float>(mousePos.y));
+					for (auto& element : buttons) {
+						if (element.getBounds().contains(mousePosF)) {
+							if (element.getName() == "Play") {
+								hoveredSelection = selection::Play;
+							}
+							else if (element.getName() == "Exit") {
+								hoveredSelection = selection::Exit;
+							}
+						}
+					}
 				}
 			}
 		}
+		// Logic
+		if (enteredSelection != selection::Nothing) {
+			switch (enteredSelection)
+			{
+			case selection::Play:
+				state = GameState::Playing;
+				exiting = true;
+			case selection::Exit:
+				state = GameState::Exiting;
+				exiting = true;
+			}
+		}
+		// Drawing
 		window.clear();
-		window.draw(rect);
-		window.draw(accountButton);
-		window.draw(accountText);
-		window.draw(pfp);
+		for (const auto& element : screenElements) {
+			window.draw(*element);
+		}
 		window.display();
 	}
 }
