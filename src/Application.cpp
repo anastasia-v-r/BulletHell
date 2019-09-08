e3d572e39a410d5ff5e7eb0e3a94ab5b8976ed01
Dominik Rutter <dominik.rutter@gmail.com>
2019-09-07 22:49:36 -0400

    run -> HandleInput,UpdateGame,DrawElements

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,16 +1,81 @@
 #include "Application.hpp"
+#include <iostream>
 
 Application::Application() {
 	auto mode = sf::VideoMode::getDesktopMode();
 	mode.height += 1;
 	mWindow.create(mode, "BulletHell", sf::Style::Default);
+	direction = Dir::None;
 }
 
 void Application::run() {
-	sf::RectangleShape box(sf::Vector2f(10, 10));
 	while (!exiting) {
-		mWindow.clear();
-		mWindow.draw(box);
-		mWindow.display();
+		HandleInput();
+		UpdateGame();
+		DrawElements();
 	}
+}
+
+void Application::HandleInput() {
+	sf::Event evnt;
+	while (mWindow.pollEvent(evnt)) {
+		switch (evnt.type)
+		{
+		case sf::Event::KeyPressed:
+			std::cout << evnt.key.code << std::endl;
+			switch (evnt.key.code) {
+			case sf::Keyboard::W:
+				std::cout << "W" << std::endl;
+				direction = Dir::Up;
+				break;
+			case sf::Keyboard::D:
+				std::cout << "D" << std::endl;
+				direction = Dir::Right;
+				break;
+			case sf::Keyboard::S:
+				std::cout << "S" << std::endl;
+				direction = Dir::Down;
+				break;
+			case sf::Keyboard::A:
+				std::cout << "A" << std::endl;
+				direction = Dir::Left;
+				break;
+			}
+			break;
+		case sf::Event::KeyReleased:
+			std::cout << "None" << std::endl;
+			direction = Dir::None;
+			break;
+		default:
+			break;
+		}
+	}
+}
+
+void Application::UpdateGame() {
+	switch (direction)
+	{
+	case None:
+		break;
+	case Up:
+		box.setPosition(box.getPosition().x, box.getPosition().y - .1);
+		break;
+	case Right:
+		box.setPosition(box.getPosition().x + .1, box.getPosition().y);
+		break;
+	case Down:
+		box.setPosition(box.getPosition().x, box.getPosition().y + .1);
+		break;
+	case Left:
+		box.setPosition(box.getPosition().x - .1, box.getPosition().y);
+		break;
+	default:
+		break;
+	}
+}
+
+void Application::DrawElements() {
+	mWindow.clear();
+	mWindow.draw(box);
+	mWindow.display();
 }
--- src/Application.hpp
+++ src/Application.hpp
@@ -3,17 +3,30 @@
 #include "ResourceManager.hpp"
 #include <SFML/Graphics.hpp>
 
+enum Dir
+{
+	None,
+	Up,
+	Right,
+	Down,
+	Left
+};
+
 class Application
 {
 public:
 	Application();
 	void run();
 private:
-
+	void HandleInput();
+	void UpdateGame();
+	void DrawElements();
 private:
 	sf::RenderWindow mWindow;
 	StateManager mStackManager;
 	ResourceManager mResourceManager;
 
 	bool exiting = false;
+	sf::RectangleShape box = sf::RectangleShape(sf::Vector2f(10, 10));
+	Dir direction;
 };
