96aff6b3b759f657a3769d67854f1334908f9390
Dominik Rutter <dominik.rutter@gmail.com>
2019-09-09 06:29:46 -0400

    Remove debugging code that breaks Linux/Mac

--- src/SplashState.cpp
+++ src/SplashState.cpp
@@ -1,11 +1,8 @@
 #include "SplashState.hpp"
-#include <filesystem>
-#include <iostream>
 
 SplashState::SplashState() {
 	auto screen = sf::VideoMode::getDesktopMode();
 	splashImage.setSize(sf::Vector2f(screen.width, screen.height));
-	std::cout << std::experimental::filesystem::current_path().string() << std::endl;
 	image.loadFromFile("assets/splashScreen/BulletHellSplash.png");
 	splashImage.setTexture(&image);
 }
