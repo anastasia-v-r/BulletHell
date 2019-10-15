d0bece1451da778f16b50305d7ffbf01ef67a54a
Dominik Rutter <dominik.rutter@gmail.com>
2019-10-15 18:33:22 -0400

    There is now a boss that shoots at you and strafes

--- src/bullet.hpp
+++ src/bullet.hpp
@@ -3,19 +3,27 @@
 class Bullet : public sf::Drawable
 {
 public:
-	Bullet(sf::Vector2f pos)
+	Bullet(sf::Vector2f pos, int dir)
 		: bullet{ sf::RectangleShape(sf::Vector2f(10.0f, 10.0f)) }
-		, speed{ 750.0f } {
+		, speed{ 750.0f }
+		, direction{ dir } {
 		bullet.setPosition(pos);
 		bullet.setFillColor(sf::Color::Green);
 	}
-	void travel(const sf::Time& time) {
-		bullet.move(0.0f, -speed * time.asSeconds());
+	// Getters
+	float getHeight() {
+		return bullet.getPosition().y;
 	}
+	// Processors
+	void travel(const sf::Time& time) {
+		bullet.move(0.0f, direction * (-speed * time.asSeconds()));
+	}
+	// Draw
 	virtual void draw(sf::RenderTarget& window, sf::RenderStates states) const {
 		window.draw(bullet, states);
 	}
 private:
 	sf::RectangleShape bullet;
 	float speed;
+	int direction;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -14,18 +14,26 @@ int main() {
 		{"Space", false }
 	};
 	// Create Render Window
-	sf::RenderWindow window(sf::VideoMode::getDesktopMode(), L"弾幕", sf::Style::Default);
+	auto mode = sf::VideoMode::getDesktopMode();
+	sf::RenderWindow window(mode, L"弾幕", sf::Style::Default);
 	window.setKeyRepeatEnabled(false);
-	// Test object
-	sf::RectangleShape box(sf::Vector2f(50.0f, 50.0f));
-	box.setFillColor(sf::Color::Red);
+	// Player
+	sf::RectangleShape player(sf::Vector2f(10.0f, 10.0f));
+	player.setPosition(mode.width / 2.0f, mode.height / 2.0f);
+	player.setFillColor(sf::Color::Red);
+	// Enemy
+	sf::RectangleShape enemy(sf::Vector2f(50.0f, 50.0f));
+	enemy.setPosition(mode.width / 2.0f, 0.0f);
 	// Bullet vector
 	std::vector<Bullet> bullets;
 	// Setup clocks
 	sf::Clock gameClock;
 	sf::Clock fixedClock;
 	sf::Time bulletTimeBank;
+	sf::Time bossTimeBank;
 	std::queue<sf::Time> fps;
+	// Misc Vars
+	bool goRight = false;
 	// Run While the window is open
 	while (window.isOpen()) {
 		// Process Events
@@ -86,31 +94,61 @@ int main() {
 		}
 		// Update Game
 		float boxSpeed = 500.0f;
-		float fireRate = 0.000f;
+		float fireRate = 0.02f;
+		float bossFireRate = 0.25f;
+		float enemySpeed = 200.0f;
 		auto elapsedTime = fixedClock.restart();
+		// Player movement
 		if (keyMap["Up"])
-			box.move(0, -boxSpeed * elapsedTime.asSeconds());
+			player.move(0, -boxSpeed * elapsedTime.asSeconds());
 		if (keyMap["Right"])
-			box.move(boxSpeed * elapsedTime.asSeconds(), 0);
+			player.move(boxSpeed * elapsedTime.asSeconds(), 0);
 		if (keyMap["Down"])
-			box.move(0, boxSpeed * elapsedTime.asSeconds());
+			player.move(0, boxSpeed * elapsedTime.asSeconds());
 		if (keyMap["Left"])
-			box.move(-boxSpeed * elapsedTime.asSeconds(), 0);
+			player.move(-boxSpeed * elapsedTime.asSeconds(), 0);
+		// Player firing
 		if (keyMap["Space"])
 			if (bulletTimeBank.asSeconds() > fireRate) {
-				bullets.push_back(Bullet(box.getPosition()));
-				bullets.push_back(Bullet(box.getPosition() + sf::Vector2f(box.getSize().x, 0.0f)));
+				bullets.push_back(Bullet(player.getPosition(), 1.0f));
+				bullets.push_back(Bullet(player.getPosition() + sf::Vector2f(player.getSize().x, 0.0f), 1.0f));
 				bulletTimeBank -= (sf::seconds)(fireRate);
 			}
 			else
 				bulletTimeBank += elapsedTime;
-		if (!bullets.empty())
+		// Process Bullets
+		if (!bullets.empty()) {
+			for (int i = 0; i < bullets.size(); i++) {
+				if (bullets[i].getHeight() < 0.1f || bullets[i].getHeight() > 1080.0f)
+					bullets.erase(bullets.begin() + i);
+			}
+			bullets.shrink_to_fit();
 			for (auto& bullet : bullets) {
 				bullet.travel(elapsedTime);
 			}
+		}
+		// Enemy movement
+		if (enemy.getPosition().x > ((mode.width / 3) * 2))
+			goRight = false;
+		else if (enemy.getPosition().x < ((mode.width / 3)))
+			goRight = true;
+		if (goRight)
+			enemy.move(enemySpeed * elapsedTime.asSeconds(), 0.0f);
+		else
+			enemy.move(-enemySpeed * elapsedTime.asSeconds(), 0.0f);
+		// Boss firing
+		if (bossTimeBank.asSeconds() > bossFireRate) {
+			for (int i = 0; i < 50; i += 10) {
+				bullets.push_back(Bullet(enemy.getPosition() + sf::Vector2f(i, 0.0f), -1.0f));
+			}
+			bossTimeBank -= (sf::seconds)(bossFireRate);
+		}
+		else
+			bossTimeBank += elapsedTime;
 		// Draw objects
 		window.clear();
-		window.draw(box);
+		window.draw(player);
+		window.draw(enemy);
 		if (!bullets.empty())
 			for (const auto& bullet : bullets) {
 				window.draw(bullet);
