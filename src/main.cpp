50995cc74d18ccf818a84e50c30a579cabee85bf
Dominik Rutter <dominik.rutter@gmail.com>
2019-10-15 00:45:53 -0400

    Movement prototype

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,48 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
-#include <conio.h>
 
 int main() {
+	// Create Render Window
 	sf::RenderWindow window;
 	window.create(sf::VideoMode::getDesktopMode(), "Danmaku", sf::Style::Default);
+	// Test object
+	sf::RectangleShape box(sf::Vector2f(50.0f, 50.0f));
+	box.setFillColor(sf::Color::Red);
+	// Run While the window is open
 	while (window.isOpen()) {
+		// Process Events
 		sf::Event evnt;
 		while (window.pollEvent(evnt)) {
-			if (evnt.type == sf::Event::KeyPressed) {
-				window.close();
+			switch (evnt.type)
+			{
+			case sf::Event::KeyPressed:
+				switch (evnt.key.code)
+				{
+				case sf::Keyboard::Escape:
+					window.close();
+					break;
+				case sf::Keyboard::W:
+					box.move(0.0f, -10.0f);
+					break;
+				case sf::Keyboard::A:
+					box.move(-10.0f, 0.0f);
+					break;
+				case sf::Keyboard::S:
+					box.move(0.0f, 10.0f);
+					break;
+				case sf::Keyboard::D:
+					box.move(10.0f, 0.0f);
+					break;
+				default:
+					break;
+				}
+			default:
+				break;
 			}
 		}
+		// Draw objects
 		window.clear();
+		window.draw(box);
 		window.display();
 	}
 }
